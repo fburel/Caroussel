efc889342747f41000853d1ffb419654d1a642ac
florian BUREL <florian.burel@mac.com>
2015-04-22 14:51:46 +0200

    Parametrage du projet Definition de l'api

---
+++ Caroussel/FBCaroussel.h
@@ -0,0 +1,30 @@
+//
+//  FBCaroussel.h
+//  Caroussel
+//
+//  Created by florian BUREL on 22/04/2015.
+//  Copyright (c) 2015 florian BUREL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@protocol FBCarousselDelegate <NSObject>
+
+@required
+
+- (UIView *) carroussel:(id)sender pageAtIndex:(int)position;
+
+@optional
+
+
+@end
+
+
+
+@interface FBCaroussel : UIView
+
+
+@property (/*readwrite, */ nonatomic, weak) id<FBCarousselDelegate> delegate;
+
+@end
--- Caroussel/ViewController.m
+++ Caroussel/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "FBCaroussel.h"
 
 @interface ViewController ()
+<FBCarousselDelegate>
+
+@property (weak, nonatomic) IBOutlet FBCaroussel *caroussel;
 
 @end
 
@@ -17,11 +21,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.caroussel.delegate = self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (UIView *)carroussel:(id)sender pageAtIndex:(int)position
+{
+    NSString * fileName = [NSString stringWithFormat:@"%d.jpg", position];
+    
+    
+    UIImage * image = [UIImage imageNamed:fileName];
+    
+    UIImageView * view = [[UIImageView alloc]initWithImage:image];
+    
+    // Garde l'aspect-ratio lors du resize de l'image
+    view.contentMode = UIViewContentModeScaleAspectFit;
+    
+    return view;
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
